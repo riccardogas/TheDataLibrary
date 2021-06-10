17b3502c0b0958f440b875c5a52b00fbb02f0872
Riccardo Bassani <riccardo.bassani@gasweb.it>
2021-06-10 17:56:24 +0200

    Aggiungere i file di progetto.

---
+++ TheDataLibrary/Class1.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TheDataLibrary.DBModels;
+
+namespace TheDataLibrary.DBModels
+{
+    public class TheRightContext : TestContext
+    {
+        public TheRightContext()
+        {
+        }
+
+        public TheRightContext(DbContextOptions<TestContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+      
+    }
+
+}
